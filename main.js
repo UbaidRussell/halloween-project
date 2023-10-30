1c24f6e0f620321de71dd6ded2b7a3fe144f9b2e
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-10-29 22:08:43 -0400

    Added javascript files

---
+++ main.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("btn")
+let div = document.getElementById("div")
+
+button.addEventListener("click", () => {
+    div.style.display = "block"
+    button.style.display = "none"
+})
