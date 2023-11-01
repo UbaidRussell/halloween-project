b9e758cecd9557dfaf6f49d10156d6eec35fb90b
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-10-31 22:08:18 -0400

    Slight changes 📦

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let button = document.getElementById("btn")
 let div = document.getElementById("div")
 
 button.addEventListener("click", () => {
-    div.style.display = "block"
-    button.style.display = "none"
+    div.style.display = "block";
+    button.style.display = "none";
 })
